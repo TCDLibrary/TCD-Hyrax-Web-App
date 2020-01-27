52fae14e98c5a38d46a32201d546cd605a3ad5f0
Joe Lakes <jlakes@tcd.ie>
2020-01-27 13:16:58 +0000

    Remove secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :migration_role, :app
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
