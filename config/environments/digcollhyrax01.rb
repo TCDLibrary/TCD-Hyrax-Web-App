3e94733b977355e02cab447c1bd3a39ae166d8fa
Joe Lakes <jlakes@tcd.ie>
2020-02-04 15:42:27 +0000

    Sets caching and debug options for production

--- config/environments/digcollhyrax01.rb
+++ config/environments/digcollhyrax01.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -26,9 +26,11 @@ Rails.application.configure do
       'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
-    config.action_controller.perform_caching = false
+    # config.action_controller.perform_caching = false
+    # config.cache_store = :null_store
 
-    config.cache_store = :null_store
+    config.action_controller.perform_caching = true
+    config.cache_store = :memory_store
   end
 
   # Don't care if the mailer can't send.
@@ -45,7 +47,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
