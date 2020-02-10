589ae33861fad04c26a064f6b2a3d9a39d432cd5
Joe Lakes <jlakes@tcd.ie>
2020-02-10 18:25:30 +0000

    Experiments with log rotation

--- config/environments/dcdevhyrax.rb
+++ config/environments/dcdevhyrax.rb
@@ -49,6 +49,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 5, 50 * 1024 * 1024)
+
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
