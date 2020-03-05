231f0b62e55ea3be83ce78fb3929cedeb1063269
Julie Allinson <julie@notch8.com>
2020-03-05 12:05:27 +0000

    specs for foxml_parser

--- spec/parsers/bulkrax/foxml_parser_spec.rb
+++ spec/parsers/bulkrax/foxml_parser_spec.rb
@@ -3,63 +3,97 @@
 require 'rails_helper'
 
 module Bulkrax
-  RSpec.describe XmlParser do
+  RSpec.describe FoxmlParser do
+    subject(:foxml_parser) { described_class.new(importer) }
+    let(:path) { './spec/fixtures/Named Collection Example_NAMED COLLECTION RECORD v3.6_20181207.xml' }
+    let(:parent) { nil }
+    let(:image_type) { 'Not Now' }
+    let(:import_type) { 'single' }
+    let(:object_type) { 'Work' }
+    let(:importer) do
+      Bulkrax::Importer.create(
+        name: 'Importer',
+        admin_set_id: AdminSet::DEFAULT_ID,
+        user_id: User.batch_user.id,
+        parser_klass: 'Bulkrax::FoxmlParser',
+        parser_fields: {
+          import_file_path: path,
+          object_type: object_type,
+          import_type: import_type,
+          image_type: image_type,
+          parent_id: parent
+        },
+        field_mapping: Bulkrax.field_mappings['Bulkrax::FoxmlParser']
+      )
+    end
+    let(:entry) do
+      Bulkrax::FoxmlEntry.create(
+        importerexporter: importer
+      )
+    end
+    let(:collection_entry) do
+      Bulkrax::FoxmlCollectionEntry.create(
+        importerexporter: importer
+      )
+    end
+
+    # Returns unless we have a parent
+    # Creates a collection entry for the parent
+    describe '#create_collections' do
+      context 'No parent specified' do
+        it 'nothing happens' do
+          expect(foxml_parser).not_to receive(:increment_counters)
+          foxml_parser.create_collections
+        end
+      end
+      context 'Parent specified' do
+        let(:parent) { Collection.create(title: ['Test Collection'], collection_type_gid: Hyrax::CollectionType.find_or_create_default_collection_type.gid).id }
+
+        it 'collection entry is created' do
+          expect(foxml_parser).to receive(:increment_counters).once
+          foxml_parser.create_collections
+        end
+
+        it 'parent source is set to id' do
+          foxml_parser.create_collections
+          expect(foxml_parser.parent.send(Bulkrax.system_identifier_field)).to eq([parent])
+        end
+      end
+    end
+
     describe '#create_works' do
-      subject(:xml_parser) { described_class.new(importer) }
-      let(:importer) { FactoryBot.create(:bulkrax_importer_xml) }
-      let(:entry) { FactoryBot.create(:bulkrax_entry, importerexporter: importer) }
-
-      before do
-        Bulkrax.source_identifier_field_mapping = { 'Bulkrax::XmlEntry' => 'DrisUnique' }
-        Bulkrax.default_work_type = 'Work'
-        Bulkrax.field_mappings['Bulkrax::XmlParser'] = {
-          'title' => { from: ['TitleLargerEntity'] },
-          'abstract' => { from: ['Abstract'] }
-        }
-
-        allow(Bulkrax::XmlEntry).to receive_message_chain(:where, :first_or_create!).and_return(entry)
-        allow(entry).to receive(:id)
-        allow(Bulkrax::ImportWorkJob).to receive(:perform_later)
+      context 'import_type set to single' do
+        it 'processes the records' do
+          expect(foxml_parser).to receive(:increment_counters).once
+          foxml_parser.create_works
+        end
       end
 
-      context 'with good data' do
+      context 'import_type set to multiple' do
+        let(:path) { './spec/fixtures/Named_Collection_Example_PARTS_RECORDS_v3.6_20181207.xml' }
+        let(:import_type) { 'multiple' }
+
+        it 'processes the records' do
+          expect(foxml_parser).to receive(:increment_counters).once
+          foxml_parser.create_works
+        end
+      end
+    end
+
+    # Returns unless we have a parent work
+    # Builds the correct array of entries and passes to the job
+    describe '#create_parent_child_relationships' do
+      let(:parent) { Work.create(title: ['Test Collection']).id }
+
+      context 'parent is a Work' do
         before do
-          importer.parser_fields = {
-            'import_file_path' => './spec/fixtures/xml/good.xml',
-            'record_element' => 'ROW'
-          }
+          foxml_parser.create_collections
+          allow(foxml_parser.importerexporter).to receive(:entries).and_return([collection_entry, entry])
         end
 
-        context 'and import_type set to multiple' do
-          before do
-            importer.parser_fields.merge!('import_type' => 'multiple')
-          end
-
-          it 'processes the line' do
-            expect(xml_parser).to receive(:increment_counters).twice
-            xml_parser.create_works
-          end
-
-          it 'counts the correct number of works and collections' do
-            expect(xml_parser.total).to eq(2)
-            expect(xml_parser.collections_total).to eq(0)
-          end
-        end
-
-        context 'and import_type set to single' do
-          before do
-            importer.parser_fields.merge!('import_type' => 'single')
-          end
-
-          it 'processes the line' do
-            expect(xml_parser).to receive(:increment_counters).once
-            xml_parser.create_works
-          end
-
-          it 'counts the correct number of works and collections' do
-            expect(xml_parser.total).to eq(1)
-            expect(xml_parser.collections_total).to eq(0)
-          end
+        it 'calls ChildRelationshipsJob with the correct parent and child' do
+          expect(Bulkrax::ChildRelationshipsJob).to receive(:perform_later).with(collection_entry.id, [entry.id], 1)
+          foxml_parser.create_parent_child_relationships
         end
       end
     end
