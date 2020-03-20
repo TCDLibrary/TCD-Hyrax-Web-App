76734e75435e05d7adcbabba1c3205c98190b2c0
Joe Lakes <jlakes@tcd.ie>
2020-03-20 10:54:24 +0000

    Changes deploy branch to master

--- config/deploy/dcdevhyrax.rb
+++ config/deploy/dcdevhyrax.rb
@@ -1,8 +1,8 @@
 set :stage, :dcdevhyrax
 set :rails_env, :dcdevhyrax
-#set :branch, "master"
+set :branch, "master"
 #set :branch, "Notch8-Batch-Ingest"
-set :branch, "2020-03-11-Change-Img-Folder-For-Imports"
+#set :branch, "2020-03-11-Change-Img-Folder-For-Imports"
 
 # Default deploy_to directory is /var/www/my_app_name. Override it:
 set :deploy_to, "/opt/app/TCD-Hyrax-Web-App"
