f2ce823445fc2b7a713837cc61961a0610368609
Joe Lakes <jlakes@tcd.ie>
2020-03-20 10:05:19 +0000

    Removes the need for symlink to image folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,8 @@ append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, "log", "tmp/derivatives", "tmp/uploads", "public/data/ingest", "public/branding", "tmp/imports", "public/data/tidy", "datastore"
+append :linked_dirs, "log", "tmp/derivatives", "tmp/uploads", "public/data/ingest", "public/branding", "tmp/imports", "public/data/tidy"
+#, "datastore"
 # JL : note this creates a symlink from public/branding to shared/public/branding
 
 #desc "Symlink shared config files"
