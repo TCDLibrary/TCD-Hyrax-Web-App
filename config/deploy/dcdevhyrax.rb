4f5117d3a17e8f8f33072c9a408ef130a177c9d5
Joe Lakes <jlakes@tcd.ie>
2020-07-09 13:34:46 +0100

    Sets dcdev branch back to master

--- config/deploy/dcdevhyrax.rb
+++ config/deploy/dcdevhyrax.rb
@@ -1,11 +1,11 @@
 set :stage, :dcdevhyrax
 set :rails_env, :dcdevhyrax
-#set :branch, "master"
+set :branch, "master"
 #set :branch, "Notch8-Batch-Ingest"
 #set :branch, "2020-03-11-Change-Img-Folder-For-Imports"
 #set :branch, "2020-03-25-Small-tweaks"
 #set :branch, "2020-05-15-UX-Homepage"
-set :branch, "2020-07-06-Change-Default-Ingest-Folder"
+#set :branch, "2020-07-06-Change-Default-Ingest-Folder"
 
 # Default deploy_to directory is /var/www/my_app_name. Override it:
 set :deploy_to, "/opt/app/TCD-Hyrax-Web-App"
