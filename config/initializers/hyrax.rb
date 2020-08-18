9734fa84b44ef74a1173f941cfe1bc35eb7449e6
Joe Lakes <jlakes@tcd.ie>
2020-08-18 14:06:06 +0100

    Experiments with order of iiif size full/600,/

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -161,7 +161,8 @@ Hyrax.config do |config|
     #Riiif::Engine.routes.url_helpers.image_url(file_id, host: base_url, size: size)
     #byebug
     if ENV['IIIF_SERVER_URL'].present?
-      ENV['IIIF_SERVER_URL'] + file_id.gsub('/', '%2F') + "/" + size + "/full/0/default.jpg"
+#      ENV['IIIF_SERVER_URL'] + file_id.gsub('/', '%2F') + "/" + size + "/full/0/default.jpg"
+      ENV['IIIF_SERVER_URL'] + file_id.gsub('/', '%2F') + "/full/" + size + "/0/default.jpg"
     else
       Riiif::Engine.routes.url_helpers.image_url(file_id, host: base_url, size: size)
     end
