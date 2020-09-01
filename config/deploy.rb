f0776600f989f359f32f05da09e37717df1552e6
Joe Lakes <jlakes@tcd.ie>
2020-09-01 12:56:31 +0100

    Attempts to fix capistrano --without development problem

--- config/deploy.rb
+++ config/deploy.rb
@@ -86,11 +86,14 @@ namespace :deploy do
   task :config_bundler do
     on roles(/.*/) do
       #execute :bundle, 'config', '--local deployment', true
-      execute :bundle, :config, '--local deployment true'
+      ##execute :bundle, :config, '--local deployment true'
       #execute :bundle, 'config', '--local', 'without', "development:test"
-      execute :bundle, :config, '--local without "development:test"'
+      ##execute :bundle, :config, '--local without "development:test"'
       #execute :bundle , 'config', '--local', 'path', shared_path.join('bundle')
-      execute :bundle, :config, "--local path #{shared_path.join('bundle')}"
+      ##execute :bundle, :config, "--local path #{shared_path.join('bundle')}"
+      execute :bundle, 'config', '--local deployment', true
+      execute :bundle, 'config', '--local', 'without', "development:test"
+      execute :bundle, 'config', '--local', 'path', shared_path.join('bundle')
     end
   end
 end
