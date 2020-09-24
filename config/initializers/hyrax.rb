f33f5ec63bdc2e794e2bf145f6e6743dde2316d6
Joe Lakes <jlakes@tcd.ie>
2020-09-24 12:59:37 +0100

    Fully qualifies the dcdev IIIF_SERVER_URL

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -162,7 +162,7 @@ Hyrax.config do |config|
     #byebug
     if ENV['IIIF_SERVER_URL'].present?
 #      ENV['IIIF_SERVER_URL'] + file_id.gsub('/', '%2F') + "/" + size + "/full/0/default.jpg"
-       ENV['IIIF_SERVER_URL'] + file_id.gsub('/', '%2F') + "/full/" + size + "/0/default.jpg"
+       IIIF_SERVER_URL + file_id.gsub('/', '%2F') + "/full/" + size + "/0/default.jpg"
     else
       Riiif::Engine.routes.url_helpers.image_url(file_id, host: base_url, size: size)
     end
@@ -183,7 +183,7 @@ Hyrax.config do |config|
     #uri = Riiif::Engine.routes.url_helpers.info_url(file_id, host: base_url)
     #uri.sub(%r{/info\.json\Z}, '')
     if ENV['IIIF_SERVER_URL'].present?
-      ENV['IIIF_SERVER_URL'] + file_id.gsub('/', '%2F')
+      IIIF_SERVER_URL + file_id.gsub('/', '%2F')
     else
       uri = Riiif::Engine.routes.url_helpers.info_url(file_id, host: base_url)
       uri.sub(%r{/info\.json\Z}, '')
