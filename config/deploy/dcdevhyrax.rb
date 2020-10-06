84a5fbbaa4d1aa4c887519e58a14c9c3b63acab9
Joe Lakes <jlakes@tcd.ie>
2020-10-06 10:39:12 +0100

    Sets dcdev deploy branch back to master

--- config/deploy/dcdevhyrax.rb
+++ config/deploy/dcdevhyrax.rb
@@ -1,8 +1,8 @@
 set :stage, :dcdevhyrax
 set :rails_env, :dcdevhyrax
-#set :branch, "master"
+set :branch, "master"
 
-set :branch, "2020-09-11-Move-ingest-tmp-folders-etc"
+#set :branch, "2020-09-11-Move-ingest-tmp-folders-etc"
 
 # Default deploy_to directory is /var/www/my_app_name. Override it:
 set :deploy_to, "/opt/app/TCD-Hyrax-Web-App"
