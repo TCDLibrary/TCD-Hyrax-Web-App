ddc630bbe2b4304ad3f66c5ab825cad32fadf841
Joe Lakes <jlakes@tcd.ie>
2020-11-25 10:04:20 +0000

    Creates skeleton fixity audit job

---
+++ app/jobs/fixity_audit_job.rb
@@ -0,0 +1,21 @@
+class FixityAuditJob < ApplicationJob
+  queue_as :fixity
+
+  def perform(*args)
+    # Do something later:
+
+    # What week number is this?
+
+    # Read fixity table for week Number
+
+    # Loop
+    #    > call FixityCheckJob
+    #    > wait for the results? run it now?
+    #    > Update the fixity audit table
+    # end loop
+
+    # send email
+
+    byebug
+  end
+end
