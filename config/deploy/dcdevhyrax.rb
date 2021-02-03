229968f92ea107de42f6fd95c55843e14d664793
Joe Lakes <jlakes@tcd.ie>
2021-02-03 12:31:01 +0000

    Sets dcdev deploy branch, repairs fedora.yml

--- config/deploy/dcdevhyrax.rb
+++ config/deploy/dcdevhyrax.rb
@@ -2,7 +2,7 @@ set :stage, :dcdevhyrax
 set :rails_env, :dcdevhyrax
 #set :branch, "master"
 
-set :branch, "2021-01-26-Analytics"
+set :branch, "2021-02-02-Notch-Universal-Viewer-Changes"
 
 # Default deploy_to directory is /var/www/my_app_name. Override it:
 set :deploy_to, "/opt/app/TCD-Hyrax-Web-App"
