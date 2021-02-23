2a859c14aa4646c5af0fe8597c0737225091925e
Joe Lakes <jlakes@tcd.ie>
2021-02-23 09:56:48 +0000

    Sets dcdev deploy branch

--- config/deploy/dcdevhyrax.rb
+++ config/deploy/dcdevhyrax.rb
@@ -1,8 +1,8 @@
 set :stage, :dcdevhyrax
 set :rails_env, :dcdevhyrax
-set :branch, "master"
+#set :branch, "master"
 
-#set :branch, "2021-02-02-Notch-Universal-Viewer-Changes"
+set :branch, "2021-02-18-Adds-New-Search-Tips-Screen"
 
 # Default deploy_to directory is /var/www/my_app_name. Override it:
 set :deploy_to, "/opt/app/TCD-Hyrax-Web-App"
