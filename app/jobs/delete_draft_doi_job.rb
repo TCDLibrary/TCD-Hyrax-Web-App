3432c2b7740128a7df481428ae525497e2c9c387
Joe Lakes <jlakes@tcd.ie>
2021-03-15 14:28:09 +0000

    Creates DOI blocker list, and checks work type before creating DOI

--- app/jobs/delete_draft_doi_job.rb
+++ app/jobs/delete_draft_doi_job.rb
@@ -7,20 +7,22 @@ class DeleteDraftDoiJob < ApplicationJob
 
   def perform(work)
 
-      url = URI(Rails.application.config.datacite_service  + 'dois/' + Rails.application.config.doi_prefix + '%2F' + work.id)
+    # If object is not a work, series or folio, do nothing
+    if work.work?
+        url = URI(Rails.application.config.datacite_service  + 'dois/' + Rails.application.config.doi_prefix + '%2F' + work.id)
 
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
- 
-      request = Net::HTTP::Delete.new(url)
-      request["Authorization"] = ENV['DATACITE_API_BASIC_AUTH']
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
 
-      response = http.request(request)
+        request = Net::HTTP::Delete.new(url)
+        request["Authorization"] = ENV['DATACITE_API_BASIC_AUTH']
 
-      if ["200", "201", '202', '204'].include? response.code
-         work.doi = ''
-      end
+        response = http.request(request)
 
+        if ["200", "201", '202', '204'].include? response.code
+           work.doi = ''
+        end
+    end 
   end
 
 end
--- app/jobs/generate_doi_job.rb
+++ app/jobs/generate_doi_job.rb
@@ -9,30 +9,36 @@ class GenerateDoiJob < ApplicationJob
   def perform(work)
 
     # If object is not a work, series or folio, do nothing
+    if work.work?
 
-    # If object has a DOI already, do nothing
-    if work.doi.blank?
-      # Build json for datacite
-      payload = JSON.generate(metadata_hash(work))
+      # check the list of bundle records, we don't want DOIs for them:
+      unless Doi_blocker.include? work.id
 
-      # Build call to datacite, credentials in environment variables
-      url = URI(Rails.application.config.datacite_service + 'dois')
+          # If object has a DOI already, do nothing
+          if work.doi.blank?
+            # Build json for datacite
+            payload = JSON.generate(metadata_hash(work))
 
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
+            # Build call to datacite, credentials in environment variables
+            url = URI(Rails.application.config.datacite_service + 'dois')
 
-      request = Net::HTTP::Post.new(url)
-      request["Content-Type"] = 'application/vnd.api+json'
-      request["Authorization"] = ENV['DATACITE_API_BASIC_AUTH']
-      request.body = payload
+            http = Net::HTTP.new(url.host, url.port)
+            http.use_ssl = true
 
-      response = http.request(request)
-       
-      puts response.read_body
-      # Check response, handle any errors, update the work with the doi value
-      # work.doi = "kjkjj" + work.id
-      if ["200", "201", '202'].include? response.code
-         work.doi = Rails.application.config.doi_org_url + Rails.application.config.doi_prefix + '/' + work.id
+            request = Net::HTTP::Post.new(url)
+            request["Content-Type"] = 'application/vnd.api+json'
+            request["Authorization"] = ENV['DATACITE_API_BASIC_AUTH']
+            request.body = payload
+
+            response = http.request(request)
+
+            puts response.read_body
+            # Check response, handle any errors, update the work with the doi value
+            # work.doi = "kjkjj" + work.id
+            if ["200", "201", '202'].include? response.code
+               work.doi = Rails.application.config.doi_org_url + Rails.application.config.doi_prefix + '/' + work.id
+            end
+          end
       end
     end
 
