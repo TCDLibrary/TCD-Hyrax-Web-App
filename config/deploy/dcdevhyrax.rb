f1d75812e1b32f34b0d48e7d25928d9ae6f14eef
Joe Lakes <jlakes@tcd.ie>
2021-07-20 12:27:48 +0100

    Sets dcdev deploy branch to 2021-05-24-MarcXML-Ingests

--- config/deploy/dcdevhyrax.rb
+++ config/deploy/dcdevhyrax.rb
@@ -1,8 +1,8 @@
 set :stage, :dcdevhyrax
 set :rails_env, :dcdevhyrax
-set :branch, "master"
+#set :branch, "master"
 
-#set :branch, "2021-05-10-Advisories"
+set :branch, "2021-05-24-MarcXML-Ingests"
 
 # Default deploy_to directory is /var/www/my_app_name. Override it:
 set :deploy_to, "/opt/app/TCD-Hyrax-Web-App"
