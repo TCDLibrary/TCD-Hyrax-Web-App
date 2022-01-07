a9f61025400420a20d9fe28f6adf1a1e905f8c05
Joe Lakes <jlakes@tcd.ie>
2022-01-07 11:14:19 +0000

    Changes deploy branch to master

--- config/deploy/dcdevhyrax.rb
+++ config/deploy/dcdevhyrax.rb
@@ -1,8 +1,8 @@
 set :stage, :dcdevhyrax
 set :rails_env, :dcdevhyrax
-#set :branch, "master"
+set :branch, "master"
 
-set :branch, "2021-11-29-DOI-Export-To-Sierra"
+#set :branch, "2021-11-29-DOI-Export-To-Sierra"
 
 # Default deploy_to directory is /var/www/my_app_name. Override it:
 set :deploy_to, "/opt/app/TCD-Hyrax-Web-App"
