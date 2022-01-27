16ed86db0df9b2aacbf5c97905ba234f78f3b643
Joe Lakes <jlakes@tcd.ie>
2022-01-27 15:24:18 +0000

    Adds test coverage

--- spec/jobs/update_image_labels_job_spec.rb
+++ spec/jobs/update_image_labels_job_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe UpdateImageLabelsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "call update image labels job" do
+    context "where some fileset exists" do
+      it "can be called and replaces fileset title" do
+
+        fs = FileSet.new
+        fs.title = ["A Title"]
+        fs.label = "ALabel.jpg"
+        fs.save
+
+        wo = Work.new
+        wo.title = ["A Title"]
+        wo.creator = ["A Creator"]
+        wo.keyword = ["A Keyword"]
+        wo.members << fs
+        wo.save
+
+        label = ImageDisplayName.new
+        label.object_id = wo.id
+        label.image_file_name = "ALabel.jpg"
+        label.image_display_text = "New Image Label"
+        label.save
+
+        result = UpdateImageLabelsJob.perform_now(wo)
+        expect(fs.title).to eq ["New Image Label"]
+
+      end
+    end
+
+ end
+
 end
--- spec/views/image_display_names/new.html.erb_spec.rb
+++ spec/views/image_display_names/new.html.erb_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "image_display_names/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    render
+  end
+
+  it "draws the image_display_names new view" do
+    expect(rendered).to have_content "Object Id: Paste excel data here:"
+    expect(rendered).to have_button('Generate Table')
+  end
+
 end
