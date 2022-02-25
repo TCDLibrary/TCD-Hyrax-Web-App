b865b1196b45d35c90bfa947b4f899c0c7332fd6
Joe Lakes <jlakes@tcd.ie>
2022-02-25 15:45:27 +0000

    Tries to fix a problem calling GenerateDoi from my controller

--- app/jobs/generate_doi_job.rb
+++ app/jobs/generate_doi_job.rb
@@ -40,7 +40,7 @@ class GenerateDoiJob < ApplicationJob
                 request["Content-Type"] = 'application/vnd.api+json'
                 request["Authorization"] = ENV['DATACITE_API_BASIC_AUTH']
                 request.body = payload
-                Rails.logger.warn('Authorization' + ENV['DATACITE_API_BASIC_AUTH'])
+                Rails.logger.warn('Authorization' + ENV['DATACITE_API_BASIC_AUTH'].to_s)
                 response = http.request(request)
                 Rails.logger.warn('RESPONSE CODE' + response.code)
 
